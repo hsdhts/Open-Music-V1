f9df56c55f13522f13c7f833db843cb3a02cb155
husada20 <husadahutasoit@gmail.com>
2023-07-09 23:22:26 +0700

    clean code

--- src/api/albums/index.js
+++ src/api/albums/index.js
@@ -2,12 +2,10 @@ const AlbumsHandler = require('./handler');
 const routes = require('./routes');
 
 module.exports = {
-  name: 'albumsapp_subm_v1',
+  name: 'albums',
   version: '1.0.0',
-  register: async (server, {
-    service,validator
-  }, ) => {
-    const albumsHandler = new AlbumsHandler(service,validator);
+  register: async (server, { service, validator }) => {
+    const albumsHandler = new AlbumsHandler(service, validator);
     server.route(routes(albumsHandler));
   },
 };
