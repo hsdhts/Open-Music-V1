75c0c70aca31db6d646cef035051673d8863797f
husada20 <husadahutasoit@gmail.com>
2023-07-09 23:15:46 +0700

    error handling declaration

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 require('dotenv', ).config();
 const Hapi = require('@hapi/hapi');
-
+const ClientError = require('./exceptions/ClientError');
 const albums = require('./api/albums');
 const AlbumsService = require('./service/postgres/AlbumsService');
 const AlbumsValidator = require('./validator/albums');
 const songs = require('./api/songs');
+
 const SongsService = require('./service/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
 
@@ -35,6 +36,35 @@ const init = async () => {
       },
     },
   ], );
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue;
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue;
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
