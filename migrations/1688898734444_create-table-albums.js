bb035b5b2095530231f4b68b86fc712c70cfd113
husada20 <husadahutasoit@gmail.com>
2023-07-09 22:25:12 +0700

    delete eslint and shorthands

--- migrations/1688898734444_create-table-albums.js
+++ migrations/1688898734444_create-table-albums.js
@@ -1,20 +1,20 @@
 exports.up = pgm => {
-    pgm.createTable('albums', {
-        id: {
-          type: 'VARCHAR(55)',
-          primaryKey: true,
-        },
-        name: {
-          type: 'TEXT',
-          notNull: true
-        },
-        year: {
-          type: 'INTEGER',
-          notNull: true,
-        }
-      });
+  pgm.createTable('albums', {
+      id: {
+        type: 'VARCHAR(55)',
+        primaryKey: true,
+      },
+      name: {
+        type: 'TEXT',
+        notNull: true
+      },
+      year: {
+        type: 'INTEGER',
+        notNull: true,
+      }
+    });
 };
 
 exports.down = pgm => {
-    pgm.dropTable('albums');
+  pgm.dropTable('albums');
 };
